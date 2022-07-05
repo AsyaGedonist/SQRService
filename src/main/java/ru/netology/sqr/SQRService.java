0e00c42108f887da6a2e6b85fb73e52276908d39
Anastasiia Tamonova <ice_asa@mail.ru>
2022-07-05 18:03:59 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int CountSquare(int start, int end) {
+
+        int result = 0;
+
+        for (int i = 10; i <= 99; i++) {
+            if (i * i < start) {
+                continue;
+            } else if (i * i > end) {
+                continue;
+            } else {
+                result++;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvSource({
+            "3,200,300",
+            "1,100,101",git
+            "4,200,300"
+    })
+    public void testSquareCount(int expected,int start,int end){
+        SQRService service = new SQRService();
+        int actual = service.CountSquare(start, end);
+        Assertions.assertEquals(expected, actual);
+    }
+}
